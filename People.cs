4a919c0bf75294f8c014af905da997b6d6b2ea81
ThanhDNguyen10 <73615064+ThanhDNguyen10@users.noreply.github.com>
2022-05-26 22:56:37 -0400

    Add files via upload

---
+++ People.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    /** 1. Write a program that demonstrates use of four basic principles of
+    object-oriented programming /Abstraction/-, /Encapsulation/-, /Inheritance/- and
+    /Polymorphism/-.
+    */
+    /** 3. Use /Encapsulation/ to keep many details private in each class.*/
+    //Encapsulation
+    public class People
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string gender;
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public int Age { get; set; }
+
+        public People(String name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
+
+
+
