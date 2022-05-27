7726f3bd75dda521179162cefa88dd68ba25842d
ThanhDNguyen10 <73615064+ThanhDNguyen10@users.noreply.github.com>
2022-05-26 23:27:32 -0400

    Add files via upload

---
+++ Assignment3/ConsoleApp6/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+/**6.Make sure to create appropriate /interfaces/ such as ICourseService, IStudentService, IInstructorService, IDepartmentService, IPersonService, IPersonService (should have
+person specific methods). IStudentService, IInstructorService should inherit from IPersonService.
+
+Person
+Calculate Age of the Person
+Calculate the Salary of the person, Use decimal for salary
+Salary cannot be negative number
+Can have multiple Addresses, should have method to get addresses
+
+Instructor
+Belongs to one Department and he can be Head of the Department
+Instructor will have added bonus salary based on his experience, calculate his
+years of experience based on Join Date
+
+Student
+Can take multiple courses
+Calculate student GPA based on grades for courses
+Each course will have grade from A to F
+
+Course
+Will have list of enrolled students
+
+Department
+Will have one Instructor as head
+Will have Budget for school year (start and end Date Time)
+Will offer list of courses
+*/
