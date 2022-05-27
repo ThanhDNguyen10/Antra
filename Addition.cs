10e7aef20ebd15d1f1008bdf9992b540c994025e
ThanhDNguyen10 <73615064+ThanhDNguyen10@users.noreply.github.com>
2022-05-26 23:04:08 -0400

    Add files via upload

---
+++ Addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public static class Addition
+    {
+        //Method overloading--compile time polymorphism
+        public static int AddNumbers (int a , int b)
+        {
+            return a + b;
+        }
+        public static double AddNumbers (double a, double b)
+        {
+            return a + b;
+        }
+        public static int AddNumbers(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
